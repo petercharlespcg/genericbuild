a6bf66689fd3110a7f46d6bdeff2dd8ace98389d
Cai <s7608130@bns.bns>
2023-02-10 10:22:04 -0500

    Initial add

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,31 @@
+node {
+    stage('SCM') {
+	echo 'Gathering code from version control'
+    git branch: '${branch}', url: 'https://github.com/petercharlespcg/JenkinsGroovy.git'
+    }
+    stage('Build') {
+        try {
+            echo 'Building....'
+            // sh 'dotnet --version'
+            sh 'pwd'
+            sh 'rm -rf ConsoleApp1/bin'
+            sh 'rm -rf ConsoleApp1/obj'
+            sh 'dotnet build ConsoleApp1'
+            echo 'Building peter new feature'
+            releasenotes(changes:"true")
+        } catch (ex) {
+            echo 'Something went wrong'
+            echo ex.toString();
+            currentBuild.result = 'FAILURE'
+        }
+        finally {
+            // cleanup
+        }
+    }
+    stage('Test') {
+        echo 'Testing....'
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
+    }
+}
